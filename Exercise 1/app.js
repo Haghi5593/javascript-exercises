1086aa5b692fda7b9138836245fbd82cb731edfd
Haghi5593 <dundun709400@gmail.com>
2026-09-08 08:51:00 -0700

    EXERCISE 1

---
+++ Exercise 1/app.js
@@ -0,0 +1,13 @@
+// EXERCISE ONE
+
+let x = 7
+let y = 3
+
+console.log(x + y)
+console.log(x - y)
+console.log(x * y)
+console.log(x / y)
+console.log(x ** y)
+console.log(x % y)
+console.log(--x)
+console.log(++y)
