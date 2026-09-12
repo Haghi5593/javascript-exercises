a1686f69f5cfca1daf3a50d48f8a709c8189978b
Haghi5593 <dundun709400@gmail.com>
2026-09-12 15:48:48 -0700

    EXERCISE 5

---
+++ Exercise 5/app.js
@@ -0,0 +1,11 @@
+// EXERCISE FIVE
+
+const add = (x, y) => {
+    return x + y
+}
+
+console.log(add(3, 8))
+console.log(add(76, 8))
+console.log(add(96, 45))
+console.log(add(757, 976))
+console.log(add(813, 996))
