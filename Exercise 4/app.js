b7718eb4bc6e1bd4d257fb43e2fcdc6e65f1a124
Haghi5593 <dundun709400@gmail.com>
2026-09-12 15:42:34 -0700

    EXERCISE 4

---
+++ Exercise 4/app.js
@@ -0,0 +1,21 @@
+// EXERCISE FOUR
+function add(x, y){
+    return x + y
+}
+
+console.log(add(3, 8))
+console.log(add(76, 8))
+console.log(add(96, 45))
+console.log(add(757, 976))
+console.log(add(813, 996))
+
+
+const adD = function(b, t){
+    return b + t
+}
+
+console.log(adD(57646, 67))
+console.log(adD(576, 97))
+console.log(adD(2334, 5465))
+console.log(adD(77466, 765))
+console.log(adD(746467, 87854))
